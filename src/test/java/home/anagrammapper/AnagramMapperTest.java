b6d9c5359f0ea50af9d8df555d108309601c322e
kons_lapi <alina.lapina@ssb.no>
2020-09-01 09:35:21 +0200

    two more tests

--- src/test/java/home/anagrammapper/AnagramMapperTest.java
+++ src/test/java/home/anagrammapper/AnagramMapperTest.java
@@ -3,6 +3,7 @@ package home.anagrammapper;
 import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
 
 import java.util.*;
 
@@ -22,4 +23,28 @@ public class AnagramMapperTest {
         assertThat("Anagrams are not mapped in the same set", output, is(expected));
     }
 
+    @Test
+    public void shouldMapNoAnagrams()
+    {
+        var input = Set.of("xyz", "klm", "abc");
+        var expected = List.of(
+                Set.of("xyz"),
+                Set.of("klm"),
+                Set.of("abc")
+        );
+
+        var output = AnagramMapper.process(input);
+
+        assertThat("No anagrams should be mapped in single value sets", output, is(expected));
+    }
+
+    @Test
+    public void emptyInput()
+    {
+        var input = new HashSet<String>();
+
+        var output = AnagramMapper.process(input);
+
+        assertEquals("Empty map expected on empty input", 0, output.size());
+    }
 }
